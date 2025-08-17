da197d19e4e83afea24f1cda5ae9479b079a3449
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2025-08-17 19:43:00 -0400

    Barebones for website

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ROUTES } from './utils/constants';
+import NavBar from '@/components/NavBar';
+
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Router>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<div />} />
+            <Route path={ROUTES.PROJECTS} element={<div />} />
+            <Route path={ROUTES.EXPERIENCE} element={<div />} />
+            <Route path={ROUTES.CONTACT} element={<div />} />
+          </Routes>
+        </main>
+      </Router>
     </>
   );
 }
