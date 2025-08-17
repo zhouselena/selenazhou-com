abaee7a656849956f22ff5ffe818aad1b9b5723d
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2025-08-16 20:33:44 -0400

    add eslint config

---
+++ eslint.config.ts
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import reactDom from 'eslint-plugin-react-dom';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      reactDom.configs.recommended,
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+]);
