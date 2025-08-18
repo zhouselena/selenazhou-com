048684eca9725222b6b565acf35aa4497cf196b7
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2025-08-17 21:58:39 -0400

    Add mobile menu

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,34 +1,46 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '@/assets/logo_sz.svg';
 import { ROUTES } from '@/utils/constants';
 import './styles.scss';
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <div className="navbar">
-      <img src={logo}></img>
-      <ul className="nav-menu">
+      <img src={logo} alt="Logo" className="logo" />
+
+      <div className={`nav-menu ${isOpen ? 'open' : ''}`}>
         <li>
-          <NavLink to={ROUTES.HOME} className="nav-link">
+          <NavLink to={ROUTES.HOME} className="nav-link" onClick={() => setIsOpen(false)}>
             home
           </NavLink>
         </li>
         <li>
-          <NavLink to={ROUTES.PROJECTS} className="nav-link">
+          <NavLink to={ROUTES.PROJECTS} className="nav-link" onClick={() => setIsOpen(false)}>
             projects
           </NavLink>
         </li>
         <li>
-          <NavLink to={ROUTES.EXPERIENCE} className="nav-link">
+          <NavLink to={ROUTES.EXPERIENCE} className="nav-link" onClick={() => setIsOpen(false)}>
             experience
           </NavLink>
         </li>
         <li>
-          <NavLink to={ROUTES.CONTACT} className="nav-link">
+          <NavLink to={ROUTES.CONTACT} className="nav-link" onClick={() => setIsOpen(false)}>
             contact
           </NavLink>
         </li>
-      </ul>
+      </div>
+
+      <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
 }
