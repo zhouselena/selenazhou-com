bfab3042a0629c56c30d3d08b27ef9b33caeab0d
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2025-08-17 20:15:13 -0400

    Deploy to CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import { writeFileSync } from 'node:fs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'add-cname',
+      closeBundle() {
+        writeFileSync('dist/CNAME', 'selenazhou.com');
+      },
+    },
+  ],
   css: {
     postcss: {
       plugins: [autoprefixer()],
